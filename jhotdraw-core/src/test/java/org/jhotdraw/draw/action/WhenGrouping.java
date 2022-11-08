130eb4f41f04fa9bae0aa291bece71d6bfe62d5d
Theis Langlands <thlan20@student.sdu.dk>
2022-11-08 12:43:29 +0100

    scenarioTest with Jgiven

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/WhenGrouping.java
@@ -0,0 +1,41 @@
+package org.jhotdraw.draw.action;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.BeforeStage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.figure.Figure;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class WhenGrouping extends Stage<WhenGrouping> {
+
+    @ExpectedScenarioState
+    @ProvidedScenarioState
+    DrawingEditor drawingEditor;
+
+    @ProvidedScenarioState
+    List<Figure> figures;
+
+    @ProvidedScenarioState
+    Set<Figure> selectedFigures;
+
+    private GroupAction groupAction;
+
+    @BeforeStage
+    public void before() {
+        groupAction = new GroupAction(drawingEditor);
+        selectedFigures = drawingEditor.getActiveView().getSelectedFigures();
+        figures = drawingEditor.getActiveView().getDrawing().getChildren();
+
+    }
+
+    WhenGrouping groupFigures() {
+        groupAction.actionPerformed(null);
+        return this;
+    }
+}
