a027833b719f1159e5ea862c30289d7312f6029b
unknown <tes_7583@hotmail.com>
2022-11-15 13:46:21 +0100

    running

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/draw/MainTest.java
@@ -0,0 +1,15 @@
+package org.jhotdraw.samples.draw;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        String[] args = {};
+        Main.main(args);
+        while (true){
+        }
+    }
+}
