f3ab5507ecc461bae89a77eba935f8868f81b11f
Theis Langlands <thlan20@student.sdu.dk>
2022-11-15 13:14:17 +0100

    combine and splitaction remove overide

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/odg/action/SplitAction.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/odg/action/SplitAction.java
@@ -66,7 +66,7 @@ public class SplitAction extends UngroupAction {
         return figures;
     }
 
-    @Override
+
     @SuppressWarnings("unchecked")
     public void groupFigures(CompositeFigure group, Collection<Figure> figures) {
         Collection<Figure> sorted = view.getDrawing().sort(figures);
