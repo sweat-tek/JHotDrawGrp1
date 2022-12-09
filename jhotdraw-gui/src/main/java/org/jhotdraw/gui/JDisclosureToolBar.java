571728921cd9d6da563c79b893a127db61c0d58d
TKalo <troelskaldau@live.dk>
2022-12-09 13:07:02 +0100

    Revert "Merge pull request #11 from sweat-tek/feature/show-hide-toolbar-section"
    This reverts commit 16ea4396fc6f7c8844c520020035a25cd9d4eb77, reversing
    changes made to d950dc26078f46b30136906dc5d7893b66b1eba6.

--- jhotdraw-gui/src/main/java/org/jhotdraw/gui/JDisclosureToolBar.java
+++ jhotdraw-gui/src/main/java/org/jhotdraw/gui/JDisclosureToolBar.java
@@ -8,7 +8,10 @@
 package org.jhotdraw.gui;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+
+import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
 import org.jhotdraw.gui.plaf.palette.PaletteToolBarUI;
 
@@ -34,108 +37,88 @@ public class JDisclosureToolBar extends JToolBar {
     }
 
     private void initComponents() {
+        GridBagConstraints gbc;
+        AbstractButton btn;
         setLayout(new GridBagLayout());
-
-        disclosureButton = disclosureButton != null ? disclosureButton : createPaletteButton();
-
-        add(disclosureButton, createDisclosureButtonGridConstraints());
-
-        putClientProperty(PaletteToolBarUI.TOOLBAR_INSETS_OVERRIDE_PROPERTY, new Insets(0, 0, 0, 0));
-
-        putClientProperty(PaletteToolBarUI.TOOLBAR_ICON_PROPERTY, new EmptyIcon(10, 8));
-    }
-
-    private JButton createPaletteButton() {
-        JButton btn = new JButton();
-        btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-        btn.setBorderPainted(false);
-        btn.setIcon(new DisclosureIcon());
-        btn.setOpaque(false);
-        btn.putClientProperty(DisclosureIcon.CURRENT_STATE_PROPERTY, 1);
-        btn.putClientProperty(DisclosureIcon.STATE_COUNT_PROPERTY, 2);
-        btn.addActionListener(e -> onPaletteButtonPressed());
-        return btn;
-    }
-
-    private void onPaletteButtonPressed() {
-        int currentState = (int) disclosureButton.getClientProperty(DisclosureIcon.CURRENT_STATE_PROPERTY);
-        int stateCount = (int) disclosureButton.getClientProperty(DisclosureIcon.STATE_COUNT_PROPERTY);
-        int newState = calculateNewState(currentState, stateCount);
-        setDisclosureState(newState);
-    }
-
-    private int calculateNewState(int currentState, int stateCount) {
-        return (currentState + 1) % stateCount;
-    }
-
-    private GridBagConstraints createDisclosureButtonGridConstraints() {
-        GridBagConstraints gbc = new GridBagConstraints();
+        gbc = new GridBagConstraints();
+        if (disclosureButton == null) {
+            btn = new JButton();
+            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
+            btn.setBorderPainted(false);
+            btn.setIcon(new DisclosureIcon());
+            btn.setOpaque(false);
+            disclosureButton = (JButton) btn;
+            disclosureButton.putClientProperty(DisclosureIcon.CURRENT_STATE_PROPERTY, 1);
+            disclosureButton.putClientProperty(DisclosureIcon.STATE_COUNT_PROPERTY, 2);
+            disclosureButton.addActionListener(new ActionListener() {
+                @FeatureEntryPoint(value = "tools-display")
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int newState = ((Integer) disclosureButton.getClientProperty(DisclosureIcon.CURRENT_STATE_PROPERTY) + 1)
+                            % (Integer) disclosureButton.getClientProperty(DisclosureIcon.STATE_COUNT_PROPERTY);
+                    setDisclosureState(newState);
+                }
+            });
+        } else {
+            btn = disclosureButton;
+        }
         gbc.gridx = 0;
         gbc.insets = new Insets(0, 1, 0, 1);
         gbc.anchor = GridBagConstraints.SOUTHWEST;
         gbc.fill = GridBagConstraints.NONE;
         gbc.weighty = 1d;
         gbc.weightx = 1d;
-        return gbc;
+        add(btn, gbc);
+        putClientProperty(PaletteToolBarUI.TOOLBAR_INSETS_OVERRIDE_PROPERTY, new Insets(0, 0, 0, 0));
+        putClientProperty(PaletteToolBarUI.TOOLBAR_ICON_PROPERTY, new EmptyIcon(10, 8));
     }
 
     public void setDisclosureStateCount(int newValue) {
-        setDisclosureStateProperty(newValue, DisclosureIcon.STATE_COUNT_PROPERTY, DISCLOSURE_STATE_COUNT_PROPERTY);
-    }
-
-    private void setDisclosureStateProperty(int newValue, String icon, String property) {
         int oldValue = getDisclosureStateCount();
-        disclosureButton.putClientProperty(icon, newValue);
-        firePropertyChange(property, oldValue, newValue);
+        disclosureButton.putClientProperty(DisclosureIcon.STATE_COUNT_PROPERTY, newValue);
+        firePropertyChange(DISCLOSURE_STATE_COUNT_PROPERTY, oldValue, newValue);
     }
 
     public void setDisclosureState(int newValue) {
-        setDisclosureStateCount(newValue);
-
-        reorganizeDisclosureButton(newValue);
-
-        validateParent();
-
-        repaint();
-
-        setDisclosureStateProperty(newValue, DisclosureIcon.CURRENT_STATE_PROPERTY, DISCLOSURE_STATE_PROPERTY);
-    }
-
-    private void reorganizeDisclosureButton(int newValue) {
+        int oldValue = getDisclosureState();
+        disclosureButton.putClientProperty(DisclosureIcon.CURRENT_STATE_PROPERTY, newValue);
         removeAll();
-
         JComponent c = getDisclosedComponent(newValue);
-
-        GridBagConstraints gbc = createDisclosureButtonGridConstraints();
+        GridBagConstraints gbc = new GridBagConstraints();
         if (c != null) {
-            add(c, createDisclosedComponentGridConstraints());
-            gbc.weightx = 0d;
-        } else {
+            gbc = new GridBagConstraints();
             gbc.gridx = 1;
+            gbc.weightx = 1d;
+            gbc.weighty = 1d;
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.anchor = GridBagConstraints.WEST;
+            add(c, gbc);
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.weightx = 0d;
+            gbc.insets = new Insets(0, 1, 0, 1);
+            gbc.weighty = 1d;
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.SOUTHWEST;
+            add(disclosureButton, gbc);
+        } else {
+            gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.weightx = 1d;
+            gbc.weighty = 1d;
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.SOUTHWEST;
+            gbc.insets = new Insets(0, 1, 0, 1);
+            add(disclosureButton, gbc);
         }
-        add(disclosureButton, gbc);
-    }
-
-    private void validateParent() {
         invalidate();
-
         Container parent = getParent();
-
         while (parent.getParent() != null && !parent.getParent().isValid()) {
             parent = parent.getParent();
         }
-
         parent.validate();
-    }
-
-    private GridBagConstraints createDisclosedComponentGridConstraints() {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.weightx = 1d;
-        gbc.weighty = 1d;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.WEST;
-        return gbc;
+        repaint();
+        firePropertyChange(DISCLOSURE_STATE_PROPERTY, oldValue, newValue);
     }
 
     public int getDisclosureStateCount() {
@@ -157,10 +140,9 @@ public class JDisclosureToolBar extends JToolBar {
      * WARNING: Do NOT modify this code. The content of this method is
      * always regenerated by the Form Editor.
      * /
-     * // <editor-fold defaultstate="collapsed" desc="Generated
-     * Code">//GEN-BEGIN:initComponents
-     * private void initComponents() {
-     * }// </editor-fold>//GEN-END:initComponents
+       // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+    }// </editor-fold>//GEN-END:initComponents
      */
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
