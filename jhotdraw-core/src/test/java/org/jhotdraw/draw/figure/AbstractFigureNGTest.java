cf3bea08da76995aa033d7400dbc3b9087bfe66a
Nicolaj Hansen <nicolaj.it@icloud.com>
2022-12-09 15:50:54 +0100

    fix: tests runs automatically

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/figure/AbstractFigureNGTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/figure/AbstractFigureNGTest.java
@@ -24,12 +24,9 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.Map;
 import org.jhotdraw.draw.AttributeKey;
-import static org.testng.Assert.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -48,15 +45,15 @@ public class AbstractFigureNGTest {
     public static void tearDownClass() throws Exception {
     }
 
-    @BeforeMethod
+    @Before
     public void setUpMethod() throws Exception {
     }
 
-    @AfterMethod
+    @After
     public void tearDownMethod() throws Exception {
     }
 
-    @Test(expectedExceptions = IllegalStateException.class)
+    @Test(expected = IllegalStateException.class)
     public void testChangedWithoutWillChange() {
         new AbstractFigureImpl().changed();
     }
