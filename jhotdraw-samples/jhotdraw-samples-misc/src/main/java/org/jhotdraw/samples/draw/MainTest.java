0b19b8632a5902b1723274e084a3031d7b2e6a73
unknown <tes_7583@hotmail.com>
2023-01-03 19:57:27 +0100

    pipeline

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/draw/MainTest.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/draw/MainTest.java
@@ -1,20 +1,6 @@
 package org.jhotdraw.samples.draw;
 
-import org.jhotdraw.api.app.Application;
-import org.jhotdraw.api.app.Disposable;
-import org.jhotdraw.api.app.View;
-import org.jhotdraw.api.gui.URIChooser;
-import org.jhotdraw.samples.draw.app.AbstractApplication;
 import org.testng.annotations.Test;
-
-import javax.swing.*;
-import java.awt.*;
-import java.beans.PropertyChangeListener;
-import java.io.IOException;
-import java.net.URI;
-
-import static org.junit.Assert.assertTrue;
-
 
 public class MainTest {
 
